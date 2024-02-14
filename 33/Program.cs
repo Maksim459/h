234c613e14ea90eb0eeb762b4e500fd7d2a6eea7
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-02-14 17:59:46 +0300

    Add files via upload

---
+++ 33/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x=14,y=1,z=5;
+        x += y - x++ * z;
+        Console.WriteLine("x={0} y={1} z={2}", x, y, z);
+        z = --x - y * 5;
+        Console.WriteLine("x={0} y={1} z={2}", x, y, z);
+        y /= x + 5 % z;
+        Console.WriteLine("x={0} y={1} z={2}", x, y, z);
+        z = x++ + y * 5;
+        Console.WriteLine("x={0} y={1} z={2}", x, y, z);
+        x = y - x++ * z;
+        Console.WriteLine("x={0} y={1} z={2}", x, y, z);
+        Console.ReadKey();
+
+    }
+}
---
+++ 44/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = default(int);
+        sbyte a = default(sbyte);
+        short n = default(short);
+        long m = default(long);
+        byte k = default(byte);
+        ushort l = default(ushort);
+        char h = default(char);
+        uint s = default(uint);
+        ulong d = default(ulong);
+        int? r = default(int?);
+        string t = default(string);
+        char q = default(char);
+        bool w = default(bool);
+        Console.WriteLine(x);
+        Console.WriteLine(a);
+        Console.WriteLine(n);
+        Console.WriteLine(m);
+        Console.WriteLine(k);
+        Console.WriteLine(l);
+        Console.WriteLine(h);
+        Console.WriteLine(s);
+        Console.WriteLine(d);
+        Console.WriteLine(r);
+        Console.WriteLine(t);
+        Console.WriteLine(q);
+        Console.WriteLine(w);
+       
+    }
+}
