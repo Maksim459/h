c4d236137eaf01e4f36a2c6f5f71620b10e3bc60
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-02-14 18:02:46 +0300

    Add files via upload

---
+++ 4.3/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] a = new int[5];
+        Random x = new Random();
+        for (int i = 0; i < a.Length; ++i)
+        {
+            a[i] = x.Next(1, 100);
+        }
+        int b = 0;
+    Flag:
+        Console.WriteLine(a[b]);
+        b++;
+        if (b < a.Length)
+        {
+            goto Flag;
+        }
+
+
+    }
+
+}
---
+++ Homework4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //n цикл
+        string a = "";
+        int n = 201;
+        while (n < 500) 
+        {
+            
+            if (n % 17 == 0) 
+            {
+             a += n+",";
+             ++n;
+             
+            }
+            else;
+            {
+             
+             ++n;
+            
+            } 
+        
+        }
+        a = a.TrimEnd(',');
+        Console.WriteLine(a);
+    }
+
+
+
+}
